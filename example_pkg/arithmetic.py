64873991fcb8c53b43d63a93e8b3a532b7e8140f
Summers, Randall J <rsummers@ou.edu>
2020-01-18 00:43:33 -0600

    Create a basic arithmetic module.

---
+++ example_pkg/arithmetic.py
@@ -0,0 +1,30 @@
+def add(x, y):
+    """The sum of two numbers.
+
+    Returns the sum `x + y`.
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """The difference of two numbers.
+
+    Returns the difference `x - y`.
+    """
+    return x - y
+
+
+def multiply(x, y):
+    """The product of two numbers.
+
+    Returns the product `x * y`.
+    """
+    return x * y
+
+
+def divide(x, y):
+    """The ratio of two numbers.
+
+    Returns the ratio `x / y`.
+    """
+    return x / y
